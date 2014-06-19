6cc2fbd9c7fdf55c0f3052754e0023c71e1c8aca
Andrey <grizzly-terror@yandex.ru>
2014-06-19 22:06:02 +0400

    Add green tests for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,8 +6,9 @@ describe "StaticPages" do
       visit '/static_pages/home';
       expect(page).to have_content('Tweets');
     end
-    it "should have title 'Tweets" do
-      expect(page).to have_title('Tweets');
+    it "should have title 'Tweets | Home'" do
+      visit '/static_pages/home';
+      expect(page).to have_title('Tweets | Home');
     end
   end
   describe "Help page" do
@@ -15,8 +16,9 @@ describe "StaticPages" do
       visit '/static_pages/help';
       expect(page).to have_content('Help');
     end
-    it "should have title 'Help'" do
-      expect(page).to have_title('Help');
+    it "should have title 'Tweets | Help'" do
+      visit '/static_pages/help';
+      expect(page).to have_title('Tweets | Help');
     end 
   end 
   describe "About page" do
@@ -24,8 +26,9 @@ describe "StaticPages" do
       visit '/static_pages/about';
       expect(page).to have_content('About us');
     end
-    it "should have title 'About'" do
-      expect(page).to have_title('About');
+    it "should have title 'Tweets | About'" do
+      visit '/static_pages/about';
+      expect(page).to have_title('Tweets | About');
     end 
   end 
 
