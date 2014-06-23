6175b16f5050394ec71b8a6bdc1d038048526c5d
Andrey <grizzly-terror@yandex.ru>
2014-06-23 16:09:46 +0400

    make template

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,41 +4,41 @@ describe "StaticPages" do
   let(:base_title){ "Tweets" }
   describe "Home page" do
     it "should have content 'home page'" do
-      visit '/static_pages/home';
+      visit root_path;
       expect(page).to have_content('home page');
     end
     it "should have title 'Tweets | Home'" do
-      visit '/static_pages/home';
+      visit root_path;
       expect(page).to have_title("#{base_title} | Home");
     end
   end
   describe "Help page" do
     it "should have content 'Help'" do
-      visit '/static_pages/help';
+      visit help_path;
       expect(page).to have_content('Help');
     end
     it "should have title 'Tweets | Help'" do
-      visit '/static_pages/help';
+      visit help_path;
       expect(page).to have_title("#{base_title} | Help");
     end 
   end 
   describe "About page" do
     it "should have content 'About us'" do
-      visit '/static_pages/about';
+      visit about_path;
       expect(page).to have_content('About us');
     end
     it "should have title 'Tweets | About'" do
-      visit '/static_pages/about';
+      visit about_path;
       expect(page).to have_title("#{base_title} | About");
     end 
   end 
   describe "Contacts page" do
     it "should have content 'to contact me'" do
-      visit '/static_pages/contacts';
+      visit contacts_path;
       expect(page).to have_content('to contact me');
     end
     it "should have title 'Tweets | Contacts'" do
-      visit '/static_pages/contacts';
+      visit contacts_path;
       expect(page).to have_title("#{base_title} | Contacts");
     end 
   end 
