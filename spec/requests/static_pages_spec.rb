de5f08832d25fc4a262afbcbb8a7dfdef7db0943
Andrey <grizzly-terror@yandex.ru>
2014-06-23 16:31:45 +0400

    Refactoring spec-test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,26 @@ require 'spec_helper'
 
 describe "StaticPages" do
   let(:base_title){ "Tweets" }
+  subject{ page }
   describe "Home page" do
-    it "should have content 'home page'" do
-      visit root_path;
-      expect(page).to have_content('home page');
-    end
-    it "should have title 'Tweets | Home'" do
-      visit root_path;
-      expect(page).to have_title("#{base_title} | Home");
-    end
+  before { visit root_path }
+    it { should have_content('home page') }
+    it { should have_title("#{base_title} | Home")}
   end
   describe "Help page" do
-    it "should have content 'Help'" do
-      visit help_path;
-      expect(page).to have_content('Help');
-    end
-    it "should have title 'Tweets | Help'" do
-      visit help_path;
-      expect(page).to have_title("#{base_title} | Help");
-    end 
+  before { visit help_path }
+    it {should have_content("Help") }
+    it {should have_title("#{base_title} | Help") }
   end 
   describe "About page" do
-    it "should have content 'About us'" do
-      visit about_path;
-      expect(page).to have_content('About us');
-    end
-    it "should have title 'Tweets | About'" do
-      visit about_path;
-      expect(page).to have_title("#{base_title} | About");
-    end 
+  before { visit about_path }
+    it {should have_content('About us')}
+    it {should have_title("#{base_title} | About") }
   end 
   describe "Contacts page" do
-    it "should have content 'to contact me'" do
-      visit contacts_path;
-      expect(page).to have_content('to contact me');
-    end
-    it "should have title 'Tweets | Contacts'" do
-      visit contacts_path;
-      expect(page).to have_title("#{base_title} | Contacts");
-    end 
+  before { visit contacts_path }
+    it {should have_content ("to contact me")}
+    it {should have_title("#{base_title} | Contacts")}
   end 
 
 
