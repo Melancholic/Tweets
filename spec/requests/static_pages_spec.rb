53c264981929a082c7967aa788403b521ec34b40
Andrey <grizzly-terror@yandex.ru>
2014-06-23 17:59:29 +0400

    Add test with click_link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,11 +19,23 @@ describe "StaticPages" do
   end 
   describe "Contacts page" do
   before { visit contacts_path }
-    it {should have_content ("to contact me")}
+    it {should have_selector('h1', text: 'to contact me') }
     it {should have_title(full_title("Contacts"))}
   end 
 
+  it "should have the right links on the Layout" do
+    visit root_path;
+    click_link("About");
+    expect(page).to have_title(full_title('About'));
+    click_link("Help");
+    expect(page).to have_title(full_title('Help'));
+    click_link("Home");   
+    expect(page).to have_title(full_title('Home'));
+    click_link("Sign Up");
+    expect(page).to have_title(full_title('Sign Up'));
+    click_link("Contacts");
+    expect(page).to have_title(full_title('Contacts'));
 
-
+  end
 end
 
