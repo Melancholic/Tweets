0af5d4de9156d409ff85996363b50cb42e4a5e3a
Andrey <grizzly-terror@yandex.ru>
2014-06-27 18:18:24 +0400

    add size to avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-def gravatar_for(user)
+def gravatar_for(user,options={size: 100})
   grav_id=Digest::MD5::hexdigest(user.email.downcase);
-  grav_url="https://secure.gravatar.com/avatar/#{grav_id}";
+  grav_url="https://secure.gravatar.com/avatar/#{grav_id}?s=#{options[:size]}";
   image_tag(grav_url, alt:user.name, class:"gravatar");
 end
 end
