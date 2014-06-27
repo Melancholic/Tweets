5ed3cd9ed447eae46ef3349323db455f0ddf7cc4
Andrey <grizzly-terror@yandex.ru>
2014-06-27 17:53:14 +0400

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params());
     if(@user.save)
+      flash[:success] = "Welcome to Tweets!";
       redirect_to(@user);
     else
       render 'new';
