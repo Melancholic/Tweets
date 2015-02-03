d31b17885f1e9771ff6804c5b77b8ffa05d6d952
Andrey <grizzly-terror@yandex.ru>
2015-02-03 10:42:32 +0400

    fix bug in verification where current_user is nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   #for not signed users
-  before_action :signed_in_user, only:[:index,:edit,:update, :destroy,:show, :following, :followers] # in app/helpers/session_helper.rb
+  before_action :signed_in_user, only:[:index,:edit,:update, :destroy,:show, :following, :followers, :verification, :sent_verification_mail] # in app/helpers/session_helper.rb
   before_action :verificated_user, only:[:index, :destroy,:show, :following, :followers]
   before_action :verificated_user_is_done, only: [:verification, :sent_verification_mail]
   #for signied users
@@ -165,7 +165,7 @@ private
   end
 
   def verificated_user_is_done
-    if (current_user.verificated?)
+    if ( current_user.verificated?)
       redirect_to(root_url);
     end
   end
