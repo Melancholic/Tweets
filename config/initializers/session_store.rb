e840b6d52e19b32a72955e7dbd7d13473d1ae7bd
Andrey <grizzly-terror@yandex.ru>
2015-02-11 22:27:49 +0400

    update to Rails 4.2 finished

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Tweets::Application.config.session_store :cookie_store, key: '_Tweets_session'
+Rails.application.config.session_store :cookie_store, key: '_tweets_session'
